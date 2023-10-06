3aee4a06d7b34b30ec4645b1b781e31453b6de3a
Tahaberk SOYSAL <berksysl.g@gmail.com>
2023-10-06 12:03:09 +0300

    bug fixed at mobil menu scroll

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const hamburgerEl = document.querySelector('.hamburger-menu');
 const mobileMenu = document.querySelector('.nav-menu-mobile');
 const closeMenuBtn = document.getElementById("closeMenu");
+const body = document.querySelector("body");
 
 hamburgerEl.addEventListener('click', ()=> {
     mobileMenu.style.display = "flex";
+    body.style.overflowY = "hidden";
 })
 
 closeMenuBtn.addEventListener('click', ()=> {
     mobileMenu.style.display = "none";
+    body.style.overflowY = "scroll";
 })
