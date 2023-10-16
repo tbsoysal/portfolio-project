d0be0dc7930bd7980397ea0607c9b0958607c333
Tahaberk SOYSAL <berksysl.g@gmail.com>
2023-10-16 14:55:17 +0300

    Add functionality to navigation links

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const hamburgerEl = document.querySelector('.hamburger-menu');
 const mobileMenu = document.querySelector('.nav-menu-mobile');
 const closeMenuBtn = document.getElementById("closeMenu");
 const body = document.querySelector("body");
+const mobileLinks = document.querySelectorAll(".mobile-link");
 
 hamburgerEl.addEventListener('click', ()=> {
     mobileMenu.style.display = "flex";
@@ -12,3 +13,8 @@ closeMenuBtn.addEventListener('click', ()=> {
     mobileMenu.style.display = "none";
     body.style.overflowY = "scroll";
 })
+
+mobileLinks.forEach(link => link.addEventListener('click', ()=>{
+    mobileMenu.style.display = "none";
+    body.style.overflowY = "scroll";
+}))
