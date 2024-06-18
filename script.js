1bf6ba7fd107888b2f9d8aecbebe86b423793962
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-06-18 19:49:36 +0300

    Make clickable circles

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const mobileMenu = document.querySelector('.nav-menu-mobile');
 const closeMenuBtn = document.getElementById("closeMenu");
 const body = document.querySelector("body");
 const mobileLinks = document.querySelectorAll(".mobile-link");
+const bullets = document.querySelectorAll(".bullet")
 
 hamburgerEl.addEventListener('click', ()=> {
     mobileMenu.style.display = "flex";
@@ -17,4 +18,9 @@ closeMenuBtn.addEventListener('click', ()=> {
 mobileLinks.forEach(link => link.addEventListener('click', ()=>{
     mobileMenu.style.display = "none";
     body.style.overflowY = "scroll";
+}))
+
+bullets.forEach(bullet => bullet.addEventListener('click', () => {
+    bullets.forEach(bullet => bullet.classList.remove("active"));
+    bullet.classList.toggle("active");
 }))
